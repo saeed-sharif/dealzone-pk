f2d9e27dd50b9afd2fce9a48801606b98bb65192
Saeed Sharif <120778953+saeed-sharif@users.noreply.github.com>
2026-06-03 23:02:07 +0500

    Delete assets directory

--- assets/config.js
+++
@@ -1,9 +0,0 @@
-window.DEALZONE_CONFIG = {
-  initialPassword: "saeed@1122",
-  whatsappNumber: "923258576612",
-  cloudinaryCloudName: "dmcw5ene0",
-  cloudinaryUploadPreset: "dealzone_products",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  categories: ["Fashion","Mobile Accessories","Laptop Accessories","Microphones","Car Accessories"]
-};
