a0e009aed7a30fe6805c7f8964c5c6ae272e2cf1
Saeed Sharif <120778953+saeed-sharif@users.noreply.github.com>
2026-06-04 20:29:00 +0500

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,20 @@
+window.DEALZONE_CONFIG = {
+  initialPassword: "saeed@1122",
+  whatsappNumber: "923258576612",
+
+  // Cloudinary configured
+  cloudinaryCloudName: "dmcw5ene0",
+  cloudinaryUploadPreset: "dealzone_products",
+
+  // Supabase configured
+  supabaseUrl: "https://afznsxafctckuunxiggf.supabase.co",
+  supabaseAnonKey: "sb_publishable_lThuGhigIyz2WkUfCzcEag__9A1TiFN",
+
+  categories: [
+    "Fashion",
+    "Mobile Accessories",
+    "Laptop Accessories",
+    "Microphones",
+    "Car Accessories"
+  ]
+};
